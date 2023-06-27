c19aafc9186e431a05cc808970a21cc301c99dbb
Ricardo Ibarra <ricardo@doitfy.com>
2023-06-26 17:39:54 -0700

    Added post endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { indexPage } from '../controllers/index.js';
-import { messagesPage } from '../controllers/messages.js';
+import { addMessage, messagesPage } from '../controllers/messages.js';
 const router = express.Router();
 
 router.get('/', indexPage);
 
 router.get('/messages', messagesPage)
+
+router.post('/messages', addMessage)
 
 export default router;
